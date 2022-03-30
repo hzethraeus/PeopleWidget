88b45de333e0386c23b0623d478e0df8ec93a0d5
hzethraeus <hzethraeus@gmail.com>
2022-03-30 17:01:47 +0200

    Se desc:
    Fixat:
    Convert sum to tablelookup
    LastHeard>2h = Ej tillg.
    Städat bort en del comments

--- src/component/PeopleCount.js
+++ src/component/PeopleCount.js
@@ -9,24 +9,39 @@ const PeopleCount = () =>{
     
     const [today, setDate] = useState(new Date()); // Save the current date to be able to trigger an update
     const [open, setOpen] = useState(true);
+    const [startTime, setStartTime] = useState(new Date());
+
     useEffect(() => {
+      
+      setDate(new Date());
+      var timeBefore= new Date();
+      timeBefore.setHours(timeBefore.getHours()-1);
+      timeBefore.setMinutes(timeBefore.getMinutes()-30);
+      setStartTime(timeBefore);
+
+
       const timer = setInterval(() => { // Creates an interval which will update the current data every minute
-        // This will trigger a rerender every component that uses the useDate hook.
-      //setDate(new Date());
-      var idag = new Date();
-      var time = idag.getHours(); //ska vara central european time.
-      if(6<time && time<22){
-          setOpen(true);
-          setDate(new Date());
-      } else {
-          setOpen(false);
-      }
-    }, 60 * 1000);
+          // This will trigger a rerender every component that uses the useDate hook.
+        //setDate(new Date());
+        var idag = new Date();
+        var time = idag.getHours(); //ska vara central european time.
+        if(6<time && time<22){
+            setOpen(true);
+            setDate(new Date());
+            var timeBefore= new Date();
+            timeBefore.setHours(timeBefore.getHours()-1);
+            timeBefore.setMinutes(timeBefore.getMinutes()-30);
+            console.log(timeBefore.toISOString());
+            setStartTime(timeBefore);
+        } else {
+            setOpen(false);
+        }
+      }, 60 * 1000);
     return () => {
       clearInterval(timer); // Return a funtion to clear the timer so that it will stop being called on unmount
     }
   }, []);
-
+  
     return(
         <Card className={styles.entire}>
             <div className={styles.floatChild}>
@@ -34,7 +49,7 @@ const PeopleCount = () =>{
                 <div className={styles.subText}>Läs mer om datan här</div>
             </div>
             <div>
-            <PeopleWidget openingHour={open} timeRN={today}/>
+            <PeopleWidget openingHour={open} timeRN={today} timeBF={startTime} />
             </div>
         </Card>
     )
--- src/component/PeopleWidget.js
+++ src/component/PeopleWidget.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useQuery, gql} from '@apollo/client';
 
 export  const PEOPLE = gql`
@@ -14,77 +14,68 @@ query Query ($timerangestart: String, $timerangeend: String){
 `;
 
 const PeopleWidget = (props) =>{
-    //var endTime = (new Date()).toISOString();
     
-   // const [timeStamp, setTimeStamp] = useState('');
-    console.log(props.timeRN.toISOString());
+   
+    console.log("time rn: " + props.timeRN.toISOString());
     var endTime = props.timeRN;
-    var startTime = new Date();
-    console.log(startTime)
-    startTime.setHours(startTime.getHours() -2);
-    console.log(startTime.toISOString());
-    console.log("hej");
-    const {data, error} = useQuery(PEOPLE, {
+    var startTime =props.timeBF;
+    console.log("time before: " + props.timeBF.toISOString());
+
+    const {data} = useQuery(PEOPLE, {
         variables: {
-            
             "timerangestart": startTime,
             "timerangeend": endTime
             },
-        //pollInterval: (60*1000), 
-    }); 
-    
-    
-    //const [peopleIn, setPeopleIn] = useState(Number(0));
-    
-    //const [tryck, setTryck]=useState('Loading');
-/*
-    useEffect(() => {
-        console.log("Uppdaterad");
-        if(data?.device.lastHeard !== timeStamp){
-            /*console.log("Inuti");
-            //console.log(typeof(data?.device.currentMeasurements[0].value));
-            if(typeof(data?.device.currentMeasurements[0].value) === 'number'){
-                setPeopleIn((peopleIn + data?.device.currentMeasurements[0].value));
+    });
+
+    var people='Ej Tillg.';
+
+    if (typeof data !== 'undefined') {
+        const obj = JSON.parse(data?.device.history);
+        console.log(obj);
+
+        const lastHeard =new Date(data?.device.lastHeard);
+        console.log(lastHeard);
+        const dif = endTime-lastHeard;
+        console.log("Dif i MS:  " + dif);
+        if(dif<5400000){
+            var sum = 0;
+            var factor=1;
+            obj.forEach(element => {
+                
+                sum+= (element.COUNTER_A*factor);
+                //factor=factor+0.1;
+                //console.log(factor);
+            });
+            console.log(sum);
+            if(sum<=6){
+                people='Lågt';
+            }else if(sum<=12){
+                people='Medel';
             }else{
-                console.log("data er ikke nummer");
+                people='Högt';
             }
-
-            setTimeStamp(data?.device.lastHeard);
-            
         }
-    },[data?.device]);
-  */  
+    }
 
+//Fixat - C - Convert sum to Tablelookup 
+//Fixat - C - Lastheard > 2h = ej tillgänglig (Ej tillg.)
+//C - Städa upp lite kod.
+//C - Hur deploy? Eventuellt snacka med Abbas
+//C - Tidszoner måste fixas
+//Factor for scaling
+//Läs mer om datan här -> Länk till Galiot.io/solutions
+//CSS ska se exakt likadan ut
+//Optimera query / caching. (Kolla på startTime/endTime)
 
-   /* useEffect(() => {
-        const timer = setInterval(() => { // Creates an interval which will update the current data every minute
-          // This will trigger a rerender every component that uses the useDate hook.
-        setPeopleIn((prev)=>{
-            return prev - prev*0.05; //Kolla upp så denna är rätt inställd.
-        });
-        if(peopleIn<=6){
-            setTryck('Lågt');
-        }else if(peopleIn<=12){
-            setTryck('Medel');
-        }else{
-            setTryck('Högt');
-        }
-      }, 60 * 2*1000);
-      return () => {
-        clearInterval(timer); // Return a funtion to clear the timer so that it will stop being called on unmount
-      }
-    }, []);*/
+//abbas@predli.com
 
-    //console.log('Hej' +timeStamp);
+    console.log("Data:");
     console.log(data);
-    console.log(error);
-    //console.log(typeof(timeStamp));
-   //console.log(peopleIn);
-    console.log(props.openingHour);
+   
     if(props.openingHour){
-        //{peopleIn} {tryck}
         
-        return <h6>Hej</h6>
+        return <h6>{people}</h6>
     }else{
 
         return <h6>Stängt</h6>
